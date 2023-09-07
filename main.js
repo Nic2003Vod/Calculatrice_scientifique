2d9d328e9b13352ce38a9ddc1b3b546f982207c2
Nicodème Sèlomin VODOUNNOU <nicodemevodounnou@gmail.com>
2023-09-07 18:09:55 +0100

    ajout des fonctionnalité de calcule, de clear

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ let output = document.getElementById('output');
 function setValue(value) {
     output.value += value;
 }
+function deleteCharacter() {
+    let inputElement = document.getElementById("output");
+    let currentValue = inputElement.value;
+    
+    if (currentValue.length > 0) {
+      let newValue = currentValue.slice(0, -1);
+      inputElement.value = newValue;
+    }
+  }
+  
+  let effButton = document.getElementById("effButton");
+  effButton.addEventListener("click", deleteCharacter);
+
+  function calculer() {
+    let inputElement = document.getElementById('output');
+    let expression = inputElement.value;
+
+    try {
+        let result = eval(expression);
+        inputElement.value = result;
+    } catch (error) {
+        inputElement.value = 'Erreur';
+    }
+}
+
+  
